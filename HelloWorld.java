421df68a464c50df858aa982264ab36a7874c42f
Thomas L <tlutz001@email.cpcc.edu>
2021-02-09 14:13:54 -0500

    New print statement added
    print statement: "program modified, but able to revert to original version using GitHub Desktop"

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
       public static void main(String[] args)
       {
-         System.out.print("Hello world!");
+         System.out.println("Hello world!");
+         System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
       }
 }
