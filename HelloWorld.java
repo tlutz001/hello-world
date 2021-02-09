c89d4324d6a9ee467d5f505ea2527f5f96c0dce9
Thomas L <tlutz001@email.cpcc.edu>
2021-02-09 14:17:18 -0500

    Revert "New print statement added"
    This reverts commit 421df68a464c50df858aa982264ab36a7874c42f.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld
 {
       public static void main(String[] args)
       {
-         System.out.println("Hello world!");
-         System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
+         System.out.print("Hello world!");
       }
 }
